123de2ff9e20804d3fad298ab1e0657443178003
Angenao <zhaopengxiang0731@gmail.com>
2024-12-30 07:22:56 +0900

    テストを通しました

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -8,5 +8,48 @@ bool quick_sort(item* begin, const item* end)
 {
 	// ToDo: クイックソートで並び替えてください
 
-	return false;
+    if (begin == NULL || end == NULL || begin >= end) 
+    {
+        return false;
+    }
+
+    item* low = begin;
+    item* high = const_cast<item*>(end - 1);
+
+    while (low < high) 
+    {
+        item pivot = *high;
+        item* i = low - 1;
+
+        for (item* j = low; j < high; j++) 
+        {
+            if (j->key <= pivot.key) 
+            {
+                i++;
+                item temp = *i;
+                *i = *j;
+                *j = temp;
+            }
+        }
+
+        item temp = *(i + 1);
+        *(i + 1) = *high;
+        *high = temp;
+
+        item* pi = i + 1;
+
+
+        if (pi - low < high - pi) 
+        {
+            quick_sort(low, pi);
+            low = pi + 1;
+        }
+        else 
+        {
+            quick_sort(pi + 1, high + 1);
+            high = pi - 1;
+        }
+    }
+
+    return true;
 }
